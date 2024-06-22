882b8608448c2d2172592837ee55d2b0be5672cb
wonny <eejoy1212@gmail.com>
2024-06-22 15:27:08 +0900

    프로모션 코드 리스트로 나오게

--- src/Components/DeviceTable.jsx
+++ src/Components/DeviceTable.jsx
@@ -6,51 +6,54 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button, Chip } from '@mui/material';
-import { getDeivceInfo } from '../apis/device';
-import { useNavigate, useParams } from 'react-router-dom';
-import { putDeleteDevice } from '../apis/device';
+import { Button, Popover, Typography } from '@mui/material';
+import { getDeivceInfo, putDeleteDevice } from '../apis/device';
+import { useNavigate } from 'react-router-dom';
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export default function DeviceTable() {
-    // const { id } = useParams();
-  const [deviceInfo,setDeviceInfo]=React.useState([])
-  const navigate=useNavigate()
-  const fetchDeviceInfo=async()=>{
-    const res=await getDeivceInfo()
-    setDeviceInfo(res)
-  }
-  React.useEffect(()=>{
+  const [deviceInfo, setDeviceInfo] = React.useState([]);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [selectedPromotionCodes, setSelectedPromotionCodes] = React.useState([]);
+  const navigate = useNavigate();
 
-fetchDeviceInfo()
-  },[])
-  const deleteDevice=async(id)=>{
-    const res= window.confirm("Are you sure you want to delete?")
+  const fetchDeviceInfo = async () => {
+    const res = await getDeivceInfo();
+    setDeviceInfo(res);
+  };
+
+  React.useEffect(() => {
+    fetchDeviceInfo();
+  }, []);
+
+  const deleteDevice = async (id) => {
+    const res = window.confirm("Are you sure you want to delete?");
     if (res) {
-      // const idx=parseInt(id)+1
-     const res=await putDeleteDevice(id)
-     if (res[1]===200) {
-      window.confirm("delete success")
+      const res = await putDeleteDevice(id);
+      if (res[1] === 200) {
+        window.confirm("delete success");
+        fetchDeviceInfo();
+      } else {
+        window.confirm("delete fail");
+      }
+    }
+  };
 
-      fetchDeviceInfo()
-     } else {
-      window.confirm("delete fail")
-     }
-    } else {
-     
-    }
-    }
+  const handleMoreClick = (event, promotionCodes) => {
+    setAnchorEl(event.currentTarget);
+    setSelectedPromotionCodes(promotionCodes);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -59,7 +62,8 @@ fetchDeviceInfo()
             <TableCell align="left">Device</TableCell>
             <TableCell align="left">Device Code</TableCell>
             <TableCell align="left">Remaining amount</TableCell>
-            <TableCell align="left">Promotion Code</TableCell>
+            <TableCell align="center" width={150}>Promotion Code</TableCell>
+            <TableCell align="left" width={250}></TableCell>
             <TableCell align="left">IP</TableCell>
             <TableCell align="left">Sales</TableCell>
             <TableCell align="left"></TableCell>
@@ -67,40 +71,69 @@ fetchDeviceInfo()
           </TableRow>
         </TableHead>
         <TableBody>
-          {deviceInfo.map((row,idx) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-       
+          {deviceInfo.map((row) => (
+            <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
               <TableCell align="left">{row.name}</TableCell>
               <TableCell align="left">{row.device_code}</TableCell>
               <TableCell align="left">{row.remaining_amount}</TableCell>
-              <TableCell align="left">{row.promotion_code}</TableCell>
+              <TableCell align="center">
+                {row.promotion_code[0]}{row.promotion_code.length > 1 ? "..." : ""}
+              </TableCell>
+              <TableCell align="left">
+                {row.promotion_code.length > 1 ? (
+                  <Button onClick={(e) => handleMoreClick(e, row.promotion_code)}>MORE+</Button>
+                ) : null}
+              </TableCell>
               <TableCell align="left">{row.ip}</TableCell>
               <TableCell align="left">{row.sales}</TableCell>
-               <TableCell align="left"><Button
-               variant='outlined'
-               sx={{
-                width:"100px"
-               }}
-               onClick={()=>{
-                sessionStorage.setItem("editRow",JSON.stringify(row))
-                navigate(`/edit-device/:${row.id}`)
-               }}
-               >Edit</Button></TableCell>
-                <TableCell align="left"><Button
-               variant='outlined'
-               color='error'
-               sx={{
-                width:"100px"
-               }}
-               onClick={()=>{deleteDevice(row.id)}}
-               >DELETE</Button></TableCell>
+              <TableCell align="left">
+                <Button
+                  variant='outlined'
+                  sx={{ width: "100px" }}
+                  onClick={() => {
+                    sessionStorage.setItem("editRow", JSON.stringify(row));
+                    navigate(`/edit-device/:${row.id}`);
+                  }}
+                >
+                  Edit
+                </Button>
+              </TableCell>
+              <TableCell align="left">
+                <Button
+                  variant='outlined'
+                  color='error'
+                  sx={{ width: "100px" }}
+                  onClick={() => { deleteDevice(row.id) }}
+                >
+                  DELETE
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Paper sx={{ p: 2 }}>
+          <Typography variant="h6">Promotion Codes</Typography>
+          {selectedPromotionCodes.slice(1).map((code, index) => (
+            <Typography
+            sx={{
+              marginTop:"20px"
+            }}
+            key={index}>{code}</Typography>
+          ))}
+        </Paper>
+      </Popover>
     </TableContainer>
   );
 }
